315be63dc8a59d1c46f0c87bab861deb7773ab1f
Diana Rogozyan <drogozyan@mines.edu>
2024-10-08 15:21:59 -0600

    Copied correct code to verification

--- verification.py
+++ verification.py
@@ -1,62 +1,73 @@
 import sys
 
-def changeToBulb(i, j, bigArray):
-    bigArray[i][j] == "L"
-    for cell in range(i+1, len(bigArray)-1):
+def changeToBulb(i, j, bigArray, lightList):
+    bigArray[i][j] = "L"
+    lightList.append((i,j))
+    for cell in range(i+1, len(bigArray)):
         if(bigArray[cell][j] == "."):
-            bigArray[cell][j] == "*"
-        elif(bigArray[cell][j] != "*"):
+            bigArray[cell][j] = "*"
+        elif(bigArray[cell][j] in "01234X"):
             break
-    for cell in range(j+1, len(bigArray[i])-1):
+    for cell in range(j+1, len(bigArray[i])):
         if(bigArray[i][cell] == "."):
-            bigArray[i][cell] == "*"
-        elif(bigArray[i][cell] != "*"):
+            bigArray[i][cell] = "*"
+        elif(bigArray[i][cell] in "01234X"):
             break
-    for cell in range(i-1, 0, -1):
+    for cell in range(i-1, -1, -1):
         if(bigArray[cell][j] == "."):
-            bigArray[cell][j] == "*"
-        elif(bigArray[cell][j] != "*"):
+            bigArray[cell][j] = "*"
+        elif(bigArray[cell][j] in "01234X"):
             break
-    for cell in range(j-1, 0, -1):
+    for cell in range(j-1, -1, -1):
         if(bigArray[i][cell] == "."):
-            bigArray[i][cell] == "*"
-        elif(bigArray[i][cell] != "*"):
+            bigArray[i][cell] = "*"
+        elif(bigArray[i][cell] in "01234X"):
             break
 
-def findLitLight(i,j,bigArray):
-    for cell in range(i+1, len(bigArray)-1):
-        if(bigArray[cell][j] == "L"):
-            bigArray[i][j] = "B"
+def findLitLight(i,j,wackyArray):
+    for cell in range(i+1, len(wackyArray)):
+        if(wackyArray[cell][j] == "L" or wackyArray[cell][j] == "B"):
+            wackyArray[i][j] = "B"
             break
-    for cell in range(j+1, len(bigArray[i])-1):
-        if(bigArray[i][cell] == "L"):
-            bigArray[i][j] = "B"
+        elif(wackyArray[cell][j] in "01234X"):
             break
-    for cell in range(i-1, 0, -1):
-        if(bigArray[cell][j] == "L"):
-            bigArray[i][j] = "B"
+    for cell in range(j+1, len(wackyArray[i])):
+        if(wackyArray[i][cell] == "L" or wackyArray[i][cell] == "B"):
+            wackyArray[i][j] = "B"
             break
-    for cell in range(j-1, 0, -1):
-        if(bigArray[i][cell] == "L"):
-            bigArray[i][j] = "B"
+        elif(wackyArray[i][cell] in "01234X"):
+            break
+    for cell in range(i-1, -1, -1):
+        if(wackyArray[cell][j] == "L" or wackyArray[cell][j] == "B"):
+            wackyArray[i][j] = "B"
+            break
+        elif(wackyArray[cell][j] in "01234X"):
+            break
+    for cell in range(j-1, -1, -1):
+        if(wackyArray[i][cell] == "L" or wackyArray[i][cell] == "B"):
+            wackyArray[i][j] = "B"
+            break
+        elif(wackyArray[i][cell] in "01234X"):
             break
 
-def verifyNumbers(i,j,bigArray,viCount):
+def verifyNumbers(i,j,bigArray):
+    viCount = 0
     bulbs = 0
     if(i != 0):
-        if(bigArray[i-1][j] in "LB"):
+        if(bigArray[i-1][j] == "L" or bigArray[i-1][j] == "B"):
             bulbs += 1
     if(i != len(bigArray)-1):
-        if(bigArray[i+1][j] in "LB"):
+        if(bigArray[i+1][j] == "L" or bigArray[i+1][j] == "B"):
             bulbs += 1
     if(j != 0):
-        if(bigArray[i][j-1] in "LB"):
+        if(bigArray[i][j-1] == "L" or bigArray[i][j-1] == "B"):
             bulbs += 1
     if(j != len(bigArray[i])-1):
-        if(bigArray[i][j+1] in "LB"):
+        if(bigArray[i][j+1] == "L" or bigArray[i][j+1] == "B"):
             bulbs += 1
     if(bulbs != int(bigArray[i][j])):
         viCount+=1
+    return viCount
 
 bigArray = []
 with open(sys.argv[1]) as input:
